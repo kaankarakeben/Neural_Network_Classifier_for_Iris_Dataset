b4ebb510e470b05a63447397e7ac77b76907d1c6
Kaan <karakebenkaan@gmail.com>
2017-11-22 00:46:50 +0000

    Commit

---
+++ sigmoid.m
@@ -0,0 +1,6 @@
+        function  sigmoid_output=sigmoid(z)
+            % change this to apply the sigmoid to the data below:
+                sigmoid_output = zeros(size(z));
+                sigmoid_output = 1.0 ./ ( 1.0 + exp(-z));
+            %sigmoid_output = 0.0;
+        end
